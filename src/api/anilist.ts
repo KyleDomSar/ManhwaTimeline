62ffa1a5e120703b73a0fb75c8700c1f1057bbdd
KyleDomSar <158052806+KyleDomSar@users.noreply.github.com>
2026-09-23 14:48:47 +0800

    Map AniList manga ratings

--- src/api/anilist.ts
+++ src/api/anilist.ts
@@ -130,7 +130,7 @@ function mapManga(item:AniListMedia):Manga {
   contentRating:"safe",
   followedCount:item.favourites??undefined,
   popularity:item.popularity??undefined,
-  favourites:item.favourites??undefined,
+  favourites:item.favourites??undefined,\n  averageScore:item.averageScore??undefined,
  };
 }
 
