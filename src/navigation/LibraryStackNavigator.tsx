8fc9fc31eb965b07aad3d95a57a38265d4762cc8
KyleDomSar <158052806+KyleDomSar@users.noreply.github.com>
2026-09-23 15:06:12 +0800

    fix: use dedicated library navigation params

--- src/navigation/LibraryStackNavigator.tsx
+++ src/navigation/LibraryStackNavigator.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { LibraryScreen } from "../screens/LibraryScreen";
 import { MangaDetailScreen } from "../screens/MangaDetailScreen";
-import type { DiscoverStackParamList } from "./DiscoverStackNavigator";
+import type { Manga } from "../types/models";
 
-const Stack=createNativeStackNavigator<DiscoverStackParamList>();
+export type LibraryStackParamList = {
+  LibraryHome: undefined;
+  MangaDetail: { manga: Manga };
+};
 
-export function LibraryStackNavigator(){
- return <Stack.Navigator screenOptions={{headerShown:false}}>
-  <Stack.Screen name="DiscoverHome" component={LibraryScreen}/>
-  <Stack.Screen name="MangaDetail" component={MangaDetailScreen}/>
- </Stack.Navigator>;
+const Stack = createNativeStackNavigator<LibraryStackParamList>();
+
+export function LibraryStackNavigator() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="LibraryHome" component={LibraryScreen} />
+      <Stack.Screen name="MangaDetail" component={MangaDetailScreen} />
+    </Stack.Navigator>
+  );
 }
